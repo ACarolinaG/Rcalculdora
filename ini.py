dc5a333e4e4d2e52f4a3a2128cbf906413117123
David Guanga <davidguanga@gmail.com>
2013-07-28 12:14:04 -0500

    Inicio Menu

---
+++ ini.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+##Caluladora Basica
+##
+##
+def calculadora():
+    while True :
+        
+        print ("---------------------------")
+        print ("|    Calculadora          |")
+        print ("---------------------------")
+        print ("-- MENU -- \n")
+        print ("Opc   Simbolo     Operacio")
+        print ("----------------------------")
+        print (" 1       +        Suma")
+        print (" 2       -        Resta")
+        print (" 3       *        Multiplicacion")
+        print (" 4       /        Divison")
+        print (" 5       x²       Cuadrado")
+        print (" 6       x³       Cubo")
+        print (" 7       %        Porciento")
+        print (" 8      mod       Modulo")
+        print (" 9      sqrt      Raiz Cudrada")
+        print (" 10      ln       Logaritmo Natural")
+        print (" 11       !       Factorial")
+        print (" 12      cos      Coseno")
+        print (" 13      seno     Seno")
+        print (" 14      tan      Tangente")
+        print (" 15      areaC    Area de un Cudrada")
+        print (" 16      areaT    Area de un Triangulo")       
+        print ("-----------------------------")
+        OP = input("Opcion -> ")
+        print ("Ok")      
+calculadora()   
